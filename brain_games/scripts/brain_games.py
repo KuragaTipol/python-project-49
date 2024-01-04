7098eb2b8ae250ef384d0b71831f85a3a081ae54
Maksim <goloviznin.08.05@gmail.com>
2024-01-04 21:58:15 +0300

    4th commit

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
+from brain_games.cli import welcome_user
+#import prompt
+
+#def welcome_user():
+#    name = prompt.string('May I have your name? ')
+#    print('Hello, {}!'.format(name))
+
 def main():
-    print('poetry run brain games')
+#    print('poetry run brain games')
     print('Welcome to the Brain Games!')
+    welcome_user()
 
 if __name__ == "__main__":
     main()
